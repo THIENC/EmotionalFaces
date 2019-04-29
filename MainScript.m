2c59c17ccfdcec9afb04a919563948add3dc8467
zhaobaotian <zhaobaotian0220@foxmail.com>
2019-04-30 00:04:13 +0800

    Update MainScript.m

--- MainScript.m
+++ MainScript.m
@@ -3,25 +3,44 @@
 % Add SPM
 
 % addpath('C:\Users\THIENC\Desktop\spm12_7219');
-addpath('D:\³ÌÐò\spm12_7219\spm12_7219');
+addpath('D:\spm12_7219');
 spm('defaults', 'EEG');
 
 % Add path
 % addpath(genpath('C:\Users\THIENC\Desktop\EmotionalFaces'))
-addpath(genpath('D:\GitHub\EmotionalFaces'))
+addpath(genpath('D:\EmotionalFaces'))
 
 % path manipulation
 
 clear
 
-
-ScriptFolder = 'D:\GitHub\EmotionalFaces';
+ScriptFolder = 'D:\EmotionalFaces';
 cd(ScriptFolder)
   
 SubjectFolder = 'E:\EmotionalFaces';
 cd(SubjectFolder)
 
-
+PatientList = [32,33,35,37:47];
+% % Move the edf files to the analysis folder
+% for i = 1:length(PatientList)
+%     cd('T:\')
+%     MainDir = dir('*0*');
+%     for j = 1:length(MainDir)
+%         if contains(MainDir(j).name,['0' num2str(PatientList(i))])
+%             mkdir(['E:\EmotionalFaces\' 'PT0' num2str(PatientList(i))])
+%             cd(MainDir(j).name)
+%             ECoGDir = dir('*ECoG');
+%             cd(ECoGDir.name)
+%             EmotionalDir = dir('Emotional*');
+%             cd(EmotionalDir.name)
+%             copyfile('*.edf',['E:\EmotionalFaces\' 'PT0' num2str(PatientList(i))])
+%             cd ..
+%             cd ..
+%             cd ..
+%         end
+%     end
+% end
+            
 %% File IO
 edfFiles = dir('*.edf');
 BehaviorData = dir('*.mat');
