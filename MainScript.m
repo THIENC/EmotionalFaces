002f6a0ee81603d38fce46042683462d84c55695
zhaobaotian <zhaobaotian0220@foxmail.com>
2019-04-30 01:03:10 +0800

    update

--- MainScript.m
+++ MainScript.m
@@ -11,7 +11,6 @@ spm('defaults', 'EEG');
 addpath(genpath('D:\EmotionalFaces'))
 
 % path manipulation
-
 clear
 
 ScriptFolder = 'D:\EmotionalFaces';
@@ -67,16 +66,15 @@ for i = 1:length(EmotionalDir)
     cd(EmotionalDir(i).name)
     edfFiles = dir('*.edf');
     BehaviorData = dir('*.mat');
-    index = [1:142];
     for loops = 1:length(edfFiles)
         
         % load the raw edf data and convert it to SPM format
-        [D,IndexInUse] = edf_TO_SPM_converter_GUI(edfFiles(loops).name,[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141]],'meeg_');
+        [D,IndexInUse] = edf_TO_SPM_converter_GUI(edfFiles(loops).name,[]'meeg_');
         
         % load and convert the DC channel
         loc_DC = index(41);
         spm('defaults', 'EEG');
-        DC = edf_TO_SPM_converter_GUI(edfFiles(loops).name,loc_DC,'DC_');
+        DC = edf_TO_SPM_converter_GUI(edfFiles(loops).name,[],'DC_');
         
         %% Channel Rename
         % Channel_Renaming_UI
